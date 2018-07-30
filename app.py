85b29cc5ca23188dad16d584c7e945638081207a
lincvic <lincvic@yahoo.com>
2018-07-30 16:31:13 +0800

    Update

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from custom_voice_Util import creatRequest, ssmlBuilder
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/trans_voice', methods=['POST', 'GET'])
+def trans_voice():
+    voiceText = request.form['voice']
+    ssml = ssmlBuilder('zh-CN', 'wangyijiang1', voiceText)
+
+    creatRequest(ssml)
+    return render_template("result.html")
+
+
+if __name__ == '__main__':
+    app.run()
